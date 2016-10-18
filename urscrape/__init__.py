60f784e28dfcbfa9ac231f49a4642476735eac1d
urwen <urwen@users.noreply.github.com>
2016-10-18 13:30:32 -0400

    Merge fix

--- urscrape/__init__.py
+++ urscrape/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< 615b6908ce30ab4bce00847071e2a1f35f403860
 #!/usr/bin/env python3
 # __init__.py -*-python-*-
 # Copyright 2016 by urwen (urwen@mail.ru)
@@ -6,7 +5,3 @@
 
 from urscrape.log import *
 DIEF_log_init()
-=======
-#!/usr/bin/python3
-# -*-python-*-
->>>>>>> Checkpoint
